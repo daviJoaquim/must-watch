758049f1c7130971be984488bf66de2b11117904
Davi Joaquim <davijoaquimamerico@gmail.com>
2026-02-18 23:58:34 +0000

    terminando must-watch

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
-from models.lista import Lista
+from flask import Flask, redirect, render_template, request, url_for
 from models.database import init_db
+from models.lista import Lista
 
 app = Flask(__name__)
-
 init_db()
 
 @app.route('/')
@@ -11,41 +10,41 @@ def home():
     return render_template('home.html', titulo='Home')
 
 @app.route('/lista', methods=['GET', 'POST'])
-def agenda():
-    listas = None
-    
+def lista():
     if request.method == 'POST':
         titulo_lista = request.form['titulo-lista']
-        categorias = request.form['categorias']
-        lista = Lista(titulo_lista, categorias)
-        lista.salvar_listas()
+        tipo_de_categoria = request.form['tipo_de_categoria']
+        indicado = request.form['indicado']
+        
+        lista = Lista(titulo_lista, tipo_de_categoria, indicado)
+        lista.salvar_lista()
 
     listas = Lista.obter_listas()
-    return render_template('lista.html', titulo='Lista de Desejos', listas=listas)
+    return render_template('lista.html', titulo='Sua Lista de Desejos', listas=listas)
 
 @app.route('/delete/<int:idLista>')
 def delete(idLista):
     lista = Lista.id(idLista)
-    lista.excluir_lista() 
-    # return render_template('agenda.html', titulo="Agenda", tarefas=tarefas)
+    lista.excluir_lista()
     return redirect(url_for('lista'))
 
-@app.route('/update/<int:idLista>', methods = ['GET', 'POST'])
+@app.route('/update/<int:idLista>', methods=['GET', 'POST'])
 def update(idLista):
-    listas = None
-        
-    if request.method == 'POST':
-        titulo = request.form['titulo-lista']
-        data = request.form['categorias']
-        lista = Lista(titulo, data, idLista)
-        lista.atualizar_lista()
-        return redirect(url_for('lista'))
-        
-    listas = Lista.obter_listas()
     lista_selecionada = Lista.id(idLista)
-    
-    return render_template('lista.html', titulo=f'Editando a lista ID: {idLista}',
-    listas=listas, lista_selecionada=lista_selecionada)
+
+    if request.method == 'POST':
+        titulo_lista = request.form['titulo-lista']
+        tipo_de_categoria = request.form['tipo_de_categoria']
+        lista = Lista(titulo_lista, tipo_de_categoria, id_lista=idLista)
+        lista.atualizar_lista()
+
+    listas = Lista.obter_listas()
+    return render_template(
+        'lista.html', 
+        titulo=f'Editando a lista ID: {idLista}',
+        lista_selecionada=lista_selecionada,
+        listas=listas
+    )
 
 @app.route('/ola')
 def ola_mundo():
--- models/lista.py
+++ models/lista.py
@@ -1,54 +1,55 @@
-from models.database import Database
-from typing import Self, Any, Optional
 from sqlite3 import Cursor
+from typing import Optional, Self, Any
+from models.database import Database
 
 class Lista:
     """
-        Classe para representar uma tarefa, com métodos para salvar, obter,
-        excluir e atualizar tarefas em um banco de dados usando a classe
-        `Database`
+        Classe Para representar atividade, com metodos para salvar, obter, excluir e  atualizar tarefas com um banco de dados usando a classe Database.
     """
-    def __init__(self: Self, titulo_lista: Optional[str], categorias: 
-    Optional[str] = None, id_lista: Optional[int] = None) -> None:
+    def __init__(self: Self, titulo_lista: Optional[str], tipo_de_categoria: Optional[str] = None ,indicado:Optional[str] = None, id_lista:Optional[int] = None)-> None:
         self.titulo_lista: Optional[str] = titulo_lista
-        self.categorias: Optional[str] = categorias
+        self.tipo_de_categoria: Optional[str]  = tipo_de_categoria
+        self.indicado: Optional[str] = indicado
         self.id_lista: Optional[int] = id_lista
-    
+        
+
     @classmethod
     def id(cls, id: int) -> Self:
         with Database() as db:
-            query: str = 'SELECT titulo_lista, categorias FROM listas WHERE id = ?;'
+            query: str = 'SELECT titulo_lista, tipo_de_categoria, indicado FROM lista WHERE id = ?;'
             params: tuple = (id,)
-            resultado: list[Any] = db.buscar_tudo(query, params)
-            
-            [[titulo, categoria]] = resultado
-            
-            return cls(id_lista=id, titulo_lista=titulo, categorias=categoria)
-    
-    def salvar_listas(self: Self) -> None:
+            resultado = db.buscar_tudo(query, params)
+            print(resultado)
+
+            [[titulo, tipo, indicado]] = resultado
+
+        return cls(id_lista=id, titulo_lista=titulo, tipo_de_atividade=tipo, indicado=indicado)
+        
+    def salvar_lista(self: Self)-> None:
         with Database() as db:
-            query: str = 'INSERT INTO listas (titulo_lista, categorias) VALUES (?, ?);'
-            params: tuple = (self.titulo_lista, self.categorias)
+            query: str = " INSERT INTO atividades (titulo_lista, tipo_de_categoria, indicado) VALUES (?, ?, ?);"
+            params: tuple = (self.titulo_lista, self.tipo_de_categoria, self.indicado)
             db.executar(query, params)
 
     @classmethod
     def obter_listas(cls) -> list[Self]:
         with Database() as db:
-            query: str = 'SELECT titulo_lista, categorias, id FROM listas;'
+            query = 'SELECT titulo_lista, categorias, id FROM listas;'
             resultados: list[Any] = db.buscar_tudo(query)
-            listas: list[Self] = [cls(titulo, categorias, id) for titulo, categorias, id in resultados]
+            listas = [cls(titulo, categorias, id_lista=id) for titulo, categorias, id in resultados]
             return listas
-        
+    
     def excluir_lista(self) -> Cursor:
         with Database() as db:
-            query: str = 'DELETE FROM listas WHERE id = ?;'
+            query: str = 'DELETE FROM atividades WHERE id = ?;'
             params: tuple = (self.id_lista,)
             resultado: Cursor = db.executar(query, params)
             return resultado
     
     def atualizar_lista(self) -> Cursor:
-        with Database() as db:
-            query: str = 'UPDATE listas SET titulo_lista = ?, categorias = ? WHERE id = ? ;'
-            params: tuple = (self.id_lista, self.titulo_lista, self.categorias)
-            resultados: Cursor = db.executar(query, params)
-            return resultados
+           with Database() as db:
+            query: str = 'UPDATE atividades SET titulo_atividade = ?, tipo_de_atividade = ?, indicado_por = ? WHERE id = ?;'
+            params: tuple = (self.titulo_lista, self.tipo_de_categoria,self.indicado, self.id_lista)
+            resultado: Cursor = db.executar(query, params)
+            return resultado
+    
