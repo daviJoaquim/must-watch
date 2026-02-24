5e399f6952f9a3742ec546f4625ea802e48f7860
davijoaquim <davijoaquimamerico@gmail.com>
2026-02-24 16:05:02 -0300

    Mostrando resultado do Must-Watch

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\AlunoADS2\Projetos\must watch\.venv"
+set "VIRTUAL_ENV=C:\Users\AlunoADS2\must-watch\.venv"
 
 if not defined PROMPT set PROMPT=$P$G
 
